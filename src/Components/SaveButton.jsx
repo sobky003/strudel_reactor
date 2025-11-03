9dd32504563dfa75cabff366d5a1d32dfa281b62
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-03 16:02:38 +1030

    Adding method to save the file in localStorage

--- src/Components/SaveButton.jsx
+++ src/Components/SaveButton.jsx
@@ -3,8 +3,14 @@
 function SaveButton({ code }) {
     const [fileName, setFileName] = useState('');
 
+    const name = fileName.trim() || 'Untitled';
+
+    const saves = JSON.parse(localStorage.getItem('strudelSaves'));
+    saves.push({ name, code })
+
+    localStorage.setItem('strudelSaves', JSON.stringify(saves));
   return (
-      <div className="d-flex flex-column align-items-start">
+      <div className="align-items-start">
           <input type="text" className="form-control mb-2" placeholder="Enter save name..." value={fileName} onChange={(e) => setFileName(e.target.value)}/>
           <button className="btn btn-success w-100" >Save Current Code</button>
       </div>
