2aaaf4930a6bf7bc710ed63964de489c2bc16432
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-03 23:30:40 +1030

    Adding comments to loadButton

--- src/Components/LoadButton.jsx
+++ src/Components/LoadButton.jsx
@@ -1,39 +1,35 @@
 import React, { useState} from 'react';
 
-
+//load the file from the localStorage
 function LoadButton({ onLoad,saves }) {
-    const [selectedIndex, setSelectedIndex] = useState(-1);
-    const [message, setMessage] = useState('');
-    const [isError, setIsError] = useState(false);
+    const [selectedIndex, setSelectedIndex] = useState(-1);//index of file selected in dropdown
+    const [message, setMessage] = useState('');//stores feedback message that will be generated
+    const [isError, setIsError] = useState(false);//to store flagged message feedback
 
 
     const Load = () => {
-        if (selectedIndex === -1) {
-            setIsError(true);
-            setMessage(' Please select a file to load.');
-            return;
-        }
-
+       //constant to get chosen file based on index selected
         const chosen = saves[selectedIndex];
 
-        if (!chosen || !chosen.code) {
-            setIsError(true);
-            setMessage('The selected file is empty or missing.');
-            return;
-        } else {
-            onLoad(chosen.code);
-            setIsError(false);
-            setMessage(` Loaded "${chosen.name}" successfully!`);
-            setTimeout(() => setMessage(''), 2000);
-        }
+        //passes code to parent to display onLoad
+        onLoad(chosen.code);
 
+        //display success feedback
+        setIsError(false);
+        setMessage(` Loaded "${chosen.name}" successfully!`)
+
+        //make message disappear after 2 sec
+        setTimeout(() => setMessage(''), 2000);
+       
     };
 
   return (
       <div className="align-items-start">
           <div className="d-flex align-items-center mb-2">
+              {/*dropdown to allow user to selecta file*/ }
               <select className="form-select mb-2" value={selectedIndex} onChange={(e) => setSelectedIndex(Number(e.target.value))}>
                   <option value={-1}>Select a saved file</option>
+                  {/*Dynamially populate dropdown with saved files*/ }
                   {saves.map((s, idx) => (
                       <option key={idx} value={idx}>
                           {s.name}
@@ -42,6 +38,7 @@ function LoadButton({ onLoad,saves }) {
               </select>
               <button className="btn btn-outline-success w-50" onClick={Load} disabled={selectedIndex === -1}>Load file</button>
           </div>
+          {/*conditional feedback message*/}
           {message && (<small className={`mt-2 d-block ${isError ? 'text-danger' : 'text-success'}`}>{message}</small>)}
       </div>
   );
