3599c679161ceb0c8b3cf21f021bf896e12b6438
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-03 23:04:41 +1030

    Adding comment to preprocessor component

--- src/Components/PreProcess.jsx
+++ src/Components/PreProcess.jsx
@@ -1,7 +1,9 @@
-function PreProcess({value, onChange }) {
+
+function PreProcess({ value, onChange }) {
   return (
       <>
           <div className="col" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+              {/*text area changes based on value passed and onchange updates the songText in parent*/ }
               <textarea className="form-control" rows="15" value={value} onChange={onChange} id="proc" ></textarea>
           </div>
       </>
