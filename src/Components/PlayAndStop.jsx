3a56cfd09e63d34e873ed4df25afa1c01f005b6b
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-03 23:09:03 +1030

    adding comments in playAndStop component

--- src/Components/PlayAndStop.jsx
+++ src/Components/PlayAndStop.jsx
@@ -1,8 +1,11 @@
 ﻿import React, { useState } from "react";
 
+//method to play and stop music
 function PlayAndStop({ onPlay, onStop }) {
+    //constant to store if music is playing
     const [isPlaying, setIsPlaying] = useState(false);
 
+    //method that plays/stop code based on isPlaying
     const PlayStopClick = () => {
         if (isPlaying) {
             onStop();
@@ -14,6 +17,7 @@ function PlayAndStop({ onPlay, onStop }) {
     }
     return (
         <div className="text-center mt-2">
+            {/*button to stop and play music. The layout changes based on if it is playing or not*/ }
             <button id="playStop" className={`${isPlaying ? "btn btn-outline-success" : "btn btn-outline-primary"}`} onClick={PlayStopClick}>
                 { isPlaying? " Stop Tune": " Play Tune" }
             </button>
