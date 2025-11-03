c0b50c3183df80763afeccd4f9859e5c4f7f2a47
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-03 16:11:04 +1030

    Fixing errors in SaveButton

--- src/Components/SaveButton.jsx
+++ src/Components/SaveButton.jsx
@@ -3,16 +3,19 @@
 function SaveButton({ code }) {
     const [fileName, setFileName] = useState('');
 
-    const name = fileName.trim() || 'Untitled';
+    const Saving = () => {
+        const name = fileName.trim() || 'Untitled';
 
-    const saves = JSON.parse(localStorage.getItem('strudelSaves'));
-    saves.push({ name, code })
+        const saves = JSON.parse(localStorage.getItem('strudelSaves')) || [];
+        saves.push({ name, code })
 
-    localStorage.setItem('strudelSaves', JSON.stringify(saves));
+        localStorage.setItem('strudelSaves', JSON.stringify(saves));
+    };
+   
   return (
       <div className="align-items-start">
           <input type="text" className="form-control mb-2" placeholder="Enter save name..." value={fileName} onChange={(e) => setFileName(e.target.value)}/>
-          <button className="btn btn-success w-100" >Save Current Code</button>
+          <button className="btn btn-success w-100" onClick={Saving} >Save Current Code</button>
       </div>
   );
 }
