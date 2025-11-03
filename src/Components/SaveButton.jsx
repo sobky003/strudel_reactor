77aaa2ca6b9b91cdb91790b3ed1f207f1bcc0050
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-03 23:22:47 +1030

    Adding comment to SaveButton

--- src/Components/SaveButton.jsx
+++ src/Components/SaveButton.jsx
@@ -1,29 +1,33 @@
 ﻿import React, { useState } from 'react';
 
-function SaveButton({ code,onSave }) {
-    const [fileName, setFileName] = useState('');
-    const [message, setMessage] = useState('');
-    const [isError, setIsError] = useState(false);
+//method to save current strudel code
+function SaveButton({ code, onSave }) {
+    const [fileName, setFileName] = useState(''); //stores text-entered by user for name
+    const [message, setMessage] = useState(''); //stores feedback message that will be generated
+    const [isError, setIsError] = useState(false); //to store flagged message feedback
 
     const Saving = () => {
+        //trim whitespace or name it untitled if no name was written
         const name = fileName.trim() || 'Untitled';
 
+        //check if the code isnt empty
         if (!code || !code.trim()) {
             setIsError(true);
             setMessage('Cannot save — no code content.');
             return;
         }
 
-        if (!name) {
-            setIsError(true);
-            setMessage('Please enter a file name before saving.');
-            return;
-        }
-
+        //calls the parent save handler with name and code
         onSave(name, code)
+
+        //provides feeback
         setIsError(false);
         setMessage(` Saved "${name}" successfully!`);
+
+        //clears input field after saving
         setFileName('');
+
+        //remove message automatically after 2 sec
         setTimeout(() => setMessage(''), 2000);
     };
    
@@ -33,6 +37,7 @@ function SaveButton({ code,onSave }) {
               <input type="text" className="form-control mb-2" placeholder="Enter save name..." value={fileName} onChange={(e) => setFileName(e.target.value)} />
               <button className="btn btn-outline-success w-50" onClick={Saving} >Save Current Code</button>
           </div>
+          {/*conditional feedback message*/ }
           {message && (<small className={`mt-2 d-block ${isError ? 'text-danger' : 'text-success'}`}>{message}</small>)}
           
       </div>
