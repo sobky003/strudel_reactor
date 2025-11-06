c9cc3e6a620e34d9dd0c237c719adbe35705862b
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-06 11:01:21 +1030

    Adding default boostrap radio layout for the toggle button

--- src/Components/ToggleTheme.jsx
+++ src/Components/ToggleTheme.jsx
@@ -1,6 +1,19 @@
 function ToggleTheme() {
   return (
-    <p>Hello world!</p>
+      <>
+          <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1"/>
+                  <label class="form-check-label" for="flexRadioDefault1">
+                      Default radio
+                  </label>
+          </div>
+          <div class="form-check">
+              <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" checked/>
+                  <label class="form-check-label" for="flexRadioDefault2">
+                      Default checked radio
+                  </label>
+          </div>
+      </>
   );
 }
 
