16531459fc30ad56ccc8630ca370d85637476157
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-18 09:48:38 +1030

    Writing code for preProcessing logix

--- src/Components/PreprocessLogic.jsx
+++ src/Components/PreprocessLogic.jsx
@@ -1,3 +1,28 @@
 export function Preprocess({ inputText, volume }) {
+    let outputText = inputText;
+
+    let regex = /[a-zA-Z0-9_]+:\s*\n[\s\S]+?\r?\n(?=[a-zA-Z0-9_]*[:\/])/gm;
+
+    let m;
+
+    let matches = []
+
+    while ((m = regex.exec(outputText)) !== null) {
+        //avoids infinite loop
+        if (m.index === regex.lastIndex) {
+            regex.lastInde++;
+        }
+
+        //results can be accessed through the 'm'-variable
+        m.forEach((match, groupIndex) => {
+            matches.push(match)
+        });
+    }
+
+    let matches2 = matches.map(
+        match => match.replaceAll(/(?<!post)gain\(([\d.]+)\)g, (match,captureGroup) => 
+            `gain(${captureGroup}*${volume})`
+        )
+    );
 
 }
