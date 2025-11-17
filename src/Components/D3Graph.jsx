7efc822ad6d92f9eaf1752ddf1281a450ebf78b5
sobky003 <sobky003@mymail.unisa.edu.au>
2025-11-17 18:36:43 +1030

    Creating starting method that builds graph by setting size of svg component

--- src/Components/D3Graph.jsx
+++ src/Components/D3Graph.jsx
@@ -5,6 +5,27 @@ export default function D3Graph({ data }) {
     //graph component
     const svgRef = useRef(null);
 
+    const buildGraph = () => {
+        //check if any data was passed and ends if no data passed.
+        if (!data || data.length === 0) return;
+
+        //remove old drawing
+        const svg = d3.select(svgRef.current);
+        svg.selectAll("*").remove(); 
+
+        //constant for margin and svg size
+        const width = 350;
+        const height = 200;
+        const margin = {
+            top: 20,
+            right: 20,
+            bottom: 30,
+            left: 40
+        };
+
+        svg.attr("width", width).attr("height", height);
+    }
+
     return (
         <div className="text-center">
             {/*display graph component*/ }
